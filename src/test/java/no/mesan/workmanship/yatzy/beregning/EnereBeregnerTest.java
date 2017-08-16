c66adc9d9af5bdd2a61c73099e13054d5b635bab
Johanne Birgitte Linde <johannel@netcompany.com>
2017-08-16 16:38:53 +0200

    Endra navn på test til å samsvare med presentasjon.

--- src/test/java/no/mesan/workmanship/yatzy/beregning/EnereBeregnerTest.java
+++ src/test/java/no/mesan/workmanship/yatzy/beregning/EnereBeregnerTest.java
@@ -7,11 +7,12 @@ import static org.junit.Assert.*;
 
 public class EnereBeregnerTest {
     @Test
-    public void skalGiNullPoengForKastUtenEnere() throws Exception {
+    public void beregnPoengForKast_medIngenEnere_skalGiNullPoeng() {
         final EnereBeregner beregner = new EnereBeregner();
+        final Kast kastUtenEnere = new Kast(2, 3, 5, 6, 4);
 
-        final Kast kastUtenEnere = new Kast(2, 3, 4, 5, 4);
-        final int kastVerdi = beregner.beregnPoengForKast(kastUtenEnere);
-        assertEquals(0, kastVerdi);
+        final int poeng = beregner.beregnPoengForKast(kastUtenEnere);
+
+        assertEquals(0, poeng);
     }
 }
